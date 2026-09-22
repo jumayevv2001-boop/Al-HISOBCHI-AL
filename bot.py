74161ba908e5c2ae9c034d0d647fab150939e318
jumayevv2001-boop <jumayevv2001@gmail.com>
2026-09-23 00:31:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, ContextTypes, filters
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
 from config import TOKEN
+
 
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text or ""
     await update.message.reply_text(f"AL HISOBCHI AI xabar oldi:\n\n{msg}")
+
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, handle))
